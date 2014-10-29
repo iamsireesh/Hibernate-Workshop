31ef5ed7fdf307497345a02907715bda532c6982
Sireesh Vattikuti <sireeshvattikuti@gmail.com>
2014-10-29 23:52:27 +0530

    Entity annotation property and Column annotation on getter or property definition example

--- src/main/java/org/sireesh/model/UserDetails.java
+++ src/main/java/org/sireesh/model/UserDetails.java
@@ -1,14 +1,16 @@
 package org.sireesh.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity
+@Entity (name="USER_DETAILS")
 public class UserDetails {
-	@Id
+	
 	private int userId;
 	private String userName;
-
+	@Id
+	@Column(name="USER_ID")
 	public int getUserId() {
 		return userId;
 	}
@@ -16,7 +18,11 @@ public class UserDetails {
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-
+	//We can use annotation on either on top of property definition or on getter
+	//We can't configure one property on getter and one property on property defination
+	//We need to maintain the uniformness to see the changes.
+	//Annotation can't be possible to configure on top of setter
+	@Column(name="USER_NAME")
 	public String getUserName() {
 		return userName;
 	}
