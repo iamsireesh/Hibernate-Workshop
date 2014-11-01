aa078924283aa4858e7e7bf4e222e05122aad4ce
Sireesh Vattikuti <sireeshvattikuti@gmail.com>
2014-11-02 01:19:15 +0530

    @AttributedsOverrides @AttributeOverride and @EmbeddedId example

--- src/main/java/org/sireesh/model/EmployeeDetails.java
+++ src/main/java/org/sireesh/model/EmployeeDetails.java
@@ -2,6 +2,8 @@ package org.sireesh.model;
 
 import java.util.Date;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -22,8 +24,17 @@ public class EmployeeDetails {
 	@Column(name = "EMP_NAME")
 	private String name;
 	@Embedded
+	@AttributeOverrides({@AttributeOverride(name="pincode", column = @Column(name="HOME_PINCODE"))})
 	// optional if we have @Embeddable annotation on Address class
-	private Address address;
+	private Address homeAddress;
+	@Embedded
+	@AttributeOverrides({@AttributeOverride(name="street", column = @Column(name="OFFICE_STREET")),
+	                    @AttributeOverride(name="city", column = @Column(name="OFFICE_CITY")),
+	                    @AttributeOverride(name="state", column = @Column(name="OFFICE_STATE")),
+	                    @AttributeOverride(name="pincode", column = @Column(name="OFFICE_PINCODE"))
+	})
+	private Address officeAddress;
+	
 	private String phone;
 	@Temporal(TemporalType.DATE)
 	private Date dob;
@@ -39,12 +50,6 @@ public class EmployeeDetails {
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
 	public String getPhone() {
 		return phone;
 	}
@@ -58,20 +63,39 @@ public class EmployeeDetails {
 		this.dob = dob;
 	}
 	
+	public Address getHomeAddress() {
+		return homeAddress;
+	}
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
+	}
 	public static void main(String[] args) {
 		EmployeeDetails emp=new EmployeeDetails();
 		emp.setName("SIREESH");
 		emp.setPhone("9XXXXXXXX9");
 		emp.setDob(new Date());
 		
-		Address address=new Address();
-		address.setCity("Hyd");
-		address.setPincode(500081);
-		address.setState("TS");
-		address.setStreet("Galli");
+		Address homeAddress=new Address();
+		homeAddress.setCity("Hyd");
+		homeAddress.setPincode(500081);
+		homeAddress.setState("TS");
+		homeAddress.setStreet("Galli");
+		
+		Address officeAddress=new Address();
+		officeAddress.setCity("Bangalore");
+		officeAddress.setPincode(500081);
+		officeAddress.setState("KA");
+		officeAddress.setStreet("Infantry");
 		
 		//set address reference
-		emp.setAddress(address);
+		emp.setHomeAddress(homeAddress);
+		emp.setOfficeAddress(officeAddress);
 		
 		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
 		Session session=sessionFactory.openSession();
